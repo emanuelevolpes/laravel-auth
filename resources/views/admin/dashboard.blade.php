fec6eea281700ecf2d9e544ea333e025b99a0df3
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-17 11:20:09 +0200

    dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,6 +3,5 @@
 @section('content')
     <div class="container text-center">
         <h1>You are logged in</h1>
-        <a href="{{ route('admin.projects.index') }}" class="btn btn-sm btn-primary">Projects list</a>
     </div>
 @endsection
