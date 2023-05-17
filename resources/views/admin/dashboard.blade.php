1edf4cd5900f4bdca41eebcd97dde039f1d31fcd
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-17 10:40:59 +0200

    link to project list

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="text-center">You are logged in</h1>
+    <div class="container text-center">
+        <h1>You are logged in</h1>
+        <a href="{{ route('admin.projects.index') }}" class="btn btn-sm btn-primary">Projects list</a>
+    </div>
 @endsection
