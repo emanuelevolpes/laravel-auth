05e3bcac62d83ee9663a3df94f6adc3392e1e274
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-17 11:59:59 +0200

    show slug controller

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -50,6 +50,7 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
+        // $project = Project::where('slug', $slug);
         return view('admin.projects.show', compact('project'));
     }
 
